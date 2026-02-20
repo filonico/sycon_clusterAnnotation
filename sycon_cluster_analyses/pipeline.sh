0d4396615dedf9cd649b205aedabf9dbbedefcca
Filippo Nicolini <fn76@alice-login01.cm.cluster>
2026-02-20 17:59:39 +0000

    update pipeline

--- sycon_cluster_analyses/pipeline.sh
+++ sycon_cluster_analyses/pipeline.sh
@@ -194,8 +194,7 @@ mkdir -p 13_recluster_blob/03_hdWGCNA/01_RNA_assay/
 Rscript scripts/22_hdWGCNA_blobOnly.R
 for i in 13_recluster_blob/03_hdWGCNA/01_RNA_assay/*ls; do grep -wf $i 09_gene_annotation/GOterms_OMA.tsv > "${i%%.*}"_GOterms.tsv; done
 for i in 13_recluster_blob/03_hdWGCNA/01_RNA_assay/*tsv; do Rscript scripts/18_perform_GOenrich.R 10_GO_enrichment/geneUniverse_GOterms.tsv $i "${i%%.*}"_; done
-for i in 12_hdWGCNA/01_RNA_assay/*ls; do Rscript scripts/19_perform_KEGGenrich.R 11_KEGG_enrichment/geneUniverse_KOterms.tsv $i "${i%%.*}"_KOenrich.tsv && echo done_$i; done
-
+for i in 13_recluster_blob/03_hdWGCNA/01_RNA_assay/*ls; do Rscript scripts/19_perform_KEGGenrich.R 11_KEGG_enrichment/geneUniverse_KOterms.tsv $i "${i%%.*}"_KOenrich.tsv && echo done_$i; done
 
 # run SAMap on re-clustered blob
 python scripts/07_run_SAMap.py -s Scil,Aque -a pairwise -i 13_recluster_blob/04_SAMap/ -o 13_recluster_blob/04_SAMap/
